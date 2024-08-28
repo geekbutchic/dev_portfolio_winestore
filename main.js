7e5ef57146cadedbdc3d976b0f3d89c0c8d23d88
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-08-28 10:57:06 -0600

    adds hamburger menu with side bar panel, focusing on mobile first design

---
+++ main.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const sideMenu = document.querySelector('.side-menu');
+
+hamburger.addEventListener('click', () => {
+  sideMenu.classList.toggle('open');
+});
